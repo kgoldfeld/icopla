3e826245d5df7918ee64e1ad7ad0582ddd838fe9
Keith Goldfeld <keith.goldfeld@nyulangone.org>
2020-09-11 10:56:50 -0400

    Slightly modifying data gen code

--- R Code/ss_data_generation.R
+++ R Code/ss_data_generation.R
@@ -3,7 +3,7 @@ library("data.table")
 
 ### Function to generate base probabilities for each study
 
-  genBaseProbs <- function(n, base, similarity, digits = 2) {
+genBaseProbs <- function(n, base, similarity, digits = 2) {
     
     # n: number of studies
     n_levels <- length(base) ## WHO level
@@ -32,23 +32,29 @@ defC <- defDataAdd(defC, varname = "b", formula = 0, variance= .5,
                    dist = "normal")                  #each study has a random slope
 defC <- defDataAdd(defC, varname = "size", formula = "75+75*large", 
                    dist = "nonrandom") 
-defC2 <- defDataAdd(varname="C_rv",formula="C * control",
-                    dist = "nonrandom") # C_rv=1/2/3: patient received control treatment C=1/2/3
-defC3 <- defDataAdd(varname = "ss", formula = "0.2;0.2;0.5;0.1", dist = "categorical")  #symptom duration strata
-defA1 <- defDataAdd(varname = "z", 
-                    formula = "0.1 * ss + (0.4 + b + (0.05 + a) * ss) * (C_rv==1) + (0.5 + b + (0.1 + a) * ss) * (C_rv==2) + (0.6 + b + (0.15 + a) * ss) * (C_rv==3)", 
-                    dist = "nonrandom")
 
-basestudy <- genBaseProbs(n = nsites,
-                          base =  c(.20, .30, .35, .15),
-                          similarity = 100)
+defC2 <- defDataAdd(
+  varname="C_rv",
+  formula="C * control",
+  dist = "nonrandom"
+) # C_rv=1/2/3: patient received control treatment C=1/2/3
 
+defC2 <- defDataAdd(defC2, varname = "ss", 
+      formula = "0.2;0.2;0.5;0.1", dist = "categorical")  #symptom duration strata
+defC2 <- defDataAdd(
+  defC2,
+  varname = "z", 
+  formula = "0.1 * ss + (0.4 + b + (0.05 + a) * ss) * (C_rv==1) + (0.5 + b + (0.1 + a) * ss) * (C_rv==2) + (0.6 + b + (0.15 + a) * ss) * (C_rv==3)", 
+  dist = "nonrandom")
 
 #### Generate data
 
 set.seed(184916)
+nsites <- 20
 
-nsites <- 12
+basestudy <- genBaseProbs(n = nsites,
+                          base =  c(.20, .30, .35, .15),
+                          similarity = 100)
 
 dstudy <- genData(nsites, id = "study")       # generate studies
 dstudy <- trtAssign(dstudy, nTrt = 3, grpName = "C") # allocate to control group
@@ -56,10 +62,10 @@ dstudy <- trtAssign(dstudy, nTrt = 2, strata = "C", grpName = "large", ratio = c
 dstudy <- addColumns(defC, dstudy)
 
 dind <- genCluster(dstudy, "study", numIndsVar = "size", "id")
-dind <- trtAssign(dind, strata="study", grpName = "control") #add control treatment variable, randomly assign control treatment in each study;rx=1:received control;rx=0:received convalescent plasma
-dind <- addColumns(defC2,dind)
-dind <- addColumns(defC3,dind)
-dind <- addColumns(defA1, dind) # add z
+# add control treatment variable, randomly assign control treatment in each study;
+# rx=1:received control;rx=0:received convalescent plasma
+dind <- trtAssign(dind, strata="study", grpName = "control") 
+dind <- addColumns(defC2, dind)
 
 dl <- lapply(1:nsites, function(i) {
   b <- basestudy[i,]
